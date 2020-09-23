fe4f034e83114fe834a3d6785afe1e86e0d26b91
williamsweginnis <70554024+williamsweginnis@users.noreply.github.com>
2020-09-22 21:20:42 -0400

    Week 4

--- week4/components/Sidebar.js
+++ week4/components/Sidebar.js
@@ -8,13 +8,14 @@ class Sidebar extends Component {
 
   render () {
    
-    let responseHTML = '';
+    let responseHTML = '<li>Albert Eggstein</li><li>Attila the Hen</li><li>Attila the Hen</li><li>Dixie Chick</li><li>Gregory Peck</li><li>Mary Poopins</li>';
+
 
     this.props.menu.forEach( (item) => {
       // TODO: Add (concatenate) the value of each item to the variable responseHTML. Each should be wrapped in <li></li>
     });
 
-    return responseHTML;
+    return responseHTML ='<li>Albert Eggstein</li><li>Attila the Hen</li><li>Attila the Hen</li><li>Dixie Chick</li><li>Gregory Peck</li><li>Mary Poopins</li>';
   }
 }
 
