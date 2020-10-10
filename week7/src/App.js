b6321dc289f5f68a4a70ce41e9cd5b09ba457fa5
williamsweginnis <70554024+williamsweginnis@users.noreply.github.com>
2020-10-10 16:42:33 -0400

    Week7

--- week7/src/App.js
+++ week7/src/App.js
@@ -10,18 +10,6 @@ let[example,changeExample]=useState(0);
       <header className="App-header">
         <p>Count:{example}</p>
           <button onClick={() => {changeExample(example + 1)}}>Increase!</button>
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
         </a>
       </header>
     </div>
