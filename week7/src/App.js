776cd67f5d00bd1622af8ed1ad45a7ec5cb56cf6
williamsweginnis <70554024+williamsweginnis@users.noreply.github.com>
2020-10-22 12:44:10 -0400

    Portfolio 1
    Changed code in week 7 to add a decrease button, as well as changed the background color, and the value in which the button increases and decreases the value of the given number.

--- week7/src/App.js
+++ week7/src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 function App() {
 
 let[example,changeExample]=useState(0);
-
   return (
     <div className="App">
       <header className="App-header">
         <p>Count:{example}</p>
-          <button onClick={() => {changeExample(example + 1)}}>Increase!</button>
+          <button onClick={() => {changeExample(example + 2)}}>Increase!</button>
+            <button onClick={() => {changeExample(example - 2)}}>Decrease!</button>
 
       </header>
     </div>
